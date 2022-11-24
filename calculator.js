b529b628d09235c41e5221b4f8a333cf5ffd095e
KeenanBILL <t19008@tsiba.ac.za>
2022-11-24 16:22:55 +0200

    Seconds

---
+++ calculator.js
@@ -0,0 +1,24 @@
+let display = getElementById('output');
+
+let buttons = Array.from(document.getElementsByClassName('buttons'));
+
+
+
+Class = new Object (
+
+)
+
+var sum = '';
+
+function addToSum(addition) {
+    console.log(sum)
+};
+
+function evaluateSum() {
+
+};
+
+function clearSum() {
+
+};
+
