d9ebc4ccf78996ed92886059c76e731f73374b53
KeenanBILL <t19008@tsiba.ac.za>
2022-11-25 14:26:55 +0200

    Well done

--- calculator.js
+++ calculator.js
@@ -1,24 +1,29 @@
-let display = getElementById('output');
+const input = document.getElementById("previous");
+const output = document.getElementById("current");
 
-let buttons = Array.from(document.getElementsByClassName('buttons'));
+function addToSum(num) {
+    input.innerHTML += num;
+}
 
-
-
-Class = new Object (
-
-)
-
-var sum = '';
-
-function addToSum(addition) {
-    console.log(sum)
-};
+function outputDisplay(num) {
+    output.innerHTML = num;
+}
 
 function evaluateSum() {
+    let equation = document.getElementById("previous") .innerText;
+    let answer = eval (equation);
+    console.log(answer);
+    outputDisplay(answer);
+}
 
-};
+function removeSum() {
+    let insert = document.getElementById("previous").innerText;
+    insert =  insert.slice(0, -1);
+    document.getElementById("previous").innerText = insert;
+    
+}
 
 function clearSum() {
-
-};
-
+    document.getElementById("previous").innerText = '';
+    document.getElementById("current").innerText = '';
+}
